5e823cac169032c98b164c274d4d59626f793675
tasyonggwapo <101799335+tasyonggwapo@users.noreply.github.com>
2022-04-25 00:42:28 +0800

    Update Board.java

--- Snake/src/Board.java
+++ Snake/src/Board.java
@@ -17,8 +17,8 @@ import javax.swing.Timer;
 
 public class Board extends JPanel implements ActionListener {
 
-    private final int B_WIDTH = 300;
-    private final int B_HEIGHT = 300;
+    private final int B_WIDTH = 330;
+    private final int B_HEIGHT = 330;
     private final int DOT_SIZE = 10;
     private final int ALL_DOTS = 900;
     private final int RAND_POS = 20;
